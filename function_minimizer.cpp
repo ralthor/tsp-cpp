64ddf6ff1a3d31afc38a8b263b7f558ec6a4b321
Amin Rezaeian <rezaeian_a@office.zharfpouyan.net>
2018-08-08 14:26:02 +0430

    added genral solution class and a function-minimizer sample

--- function_minimizer.cpp
+++ function_minimizer.cpp
@@ -1,16 +1,35 @@
+#include <cstdlib>
 #include "function_minimizer.h"
-
-//FunctionMinimizer::FunctionMinimizer()
-//{
-//    //ctor
-//}
-
-//FunctionMinimizer::~FunctionMinimizer()
-//{
-//    //dtor
-//}
 
 double FunctionMinimizer::fitness()
 {
-    return 0;
+    return value * value + 10;
+}
+
+void FunctionMinimizer::setValue(Solution<double> *s) {
+    value = s->getValue();
+}
+
+void FunctionMinimizer::mutate(double) {
+    value += (double) std::rand() / RAND_MAX - 0.5;
+}
+
+
+void FunctionMinimizer::crossover(Solution<double> *s) {
+    double p = (double) std::rand() / RAND_MAX;
+    value = value * p + s->getValue() * (1 - p);
+}
+
+
+int FunctionMinimizer::isBetterThan(Solution<double> *s) {
+    return fitness() < s->fitness();
+}
+
+
+void FunctionMinimizer::randomize() {
+    value = (double) std::rand() / RAND_MAX * 10;
+}
+
+double FunctionMinimizer::getValue() {
+    return value;
 }
--- solution.h
+++ solution.h
@@ -7,15 +7,14 @@ template <class T>
 class Solution
 {
     public:
-        Solution();
-        Solution(T);
-        //~Solution();
-        double fitness();
-        std::string serialize();
-        void set(T);
-        T get();
-
-    private:
+        virtual double fitness();
+        virtual void setValue(Solution *);
+        virtual void mutate(double); // probability is given as the parameter
+        virtual void crossover(Solution *);
+        virtual int isBetterThan(Solution *);
+        virtual void randomize();
+        virtual T getValue();
+    protected:
         T value;
 };
 
